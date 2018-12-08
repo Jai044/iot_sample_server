87d5ab1021389fa7c2585fbcf194cbdc305df7b3
Jai Sindhu <JaiSindhu@Neels-MacBook-Pro.local>
2018-12-08 17:31:51 +0530

    new commit

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ import time
 
 app = Bottle(__name__)
 
-client = MongoClient('')
-db = client.db_name
+#client = MongoClient('')
+#db = client.db_name
 
 @app.hook('after_request')
 def enable_cors():
@@ -21,19 +21,4 @@ def enable_cors():
 
 @app.route('/')
 def root():
-	return {'data': 'Server root'}
-
-@app.route('/led')
-def led():
-
-	cur = db.led.find()
-	data = json.loads(dumps(cur))
-	
-	return data[0]['val']
-
-@app.route('/led/<val>')
-def led(val):
-
-	cur = db.led.update({"_id": ObjectId("")}, {'$set': {'val': str(val)}})
-
-	return "LED Val: " + str(val)
+	return 'My Sample App'
